051c277b9e8e15c6944e1a8e159d0516fa791747
Paul Ashley <throwaway-twitter@users.noreply.github.com>
2020-12-30 11:38:55 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # twitter_task
+
+# ASSUMPTIONS
+1. The bulk of the task was to find a way to circumvent the authentication requirements from the official API's since a core requirement was "Make sure to use scraping or APIs that do not require user authentication or a twitter developer account." At the time of writing,v1.1 and v2 of the API both require user authentication. This means that a more elegant and strictly supported solution was sacrificed in adherence to the core requirement.
+2. Only the tweet text and createtime is important to display to stdout. The API option will however print the full details
+3. "new tweets" are assumed to be those identified by the default set of options from the advanced-search web API (https://twitter.com/search-advanced). Further details can be added/tweaked with modification to the search_options array.
+4. Assume in the 10 minute window, less than 100 tweets are made. If more than that, only the most recent 100 will be shown. This can be changed relatively easily by adding cursoring to the requests, however this was not seen as particularly important since the most active twitter user with > 50k followers only outputs ~135 tweets/day (source: https://sysomos.com/inside-twitter/most-active-twitter-user-data/)
+5. Twitter's search service and, by extension, the Search API is not meant to be an exhaustive source of Tweets. Not all Tweets will be indexed or made available via the search interface --- https://developer.twitter.com/en/docs/twitter-api/v1/tweets/search/api-reference/get-search-tweets
+6. Tweet collection is assumed to have a negligible response time and is not factored into the 10 minute collection (i.e. if tweet collection always took 1 second, the program would actually run 10 minutes and 1 second apart)
+7. To avoid confusion with updated tweets pushing more recent items to the bottom of stdout, tweets will be printed from least recent to most recent. 
---
+++ tweet_reader.py
@@ -0,0 +1,119 @@
+#  Inspiration for the task taken from # https://code.recuweb.com/2015/scraping-tweets-directly-from-twitter-without-authentication/#twitters-advanced-search-without-being-logged
+
+import json
+import re
+from urllib.parse import urlencode
+from urllib.parse import quote
+import requests
+import datetime
+import time
+import argparse
+
+MINUTES_TO_SECONDS = 60
+
+def get_user_agent():
+    """
+    Adds a fake user agent to the request in order to avoid twitter blocking the search request
+    :return: string (agent details)
+    """
+    return "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36"
+
+
+session = requests.Session()
+session.headers.update(
+    {'User-Agent': get_user_agent(),
+     })
+
+
+def get_auth_token():
+    """
+    Twitter looks to use the same auth token for all unauthenticated users
+    :return: string (bearer token)
+    """
+    return "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA"
+
+
+def get_guest_token():
+    """
+    Make a generic call to twitter.com to get a guest token to use for their api
+    :return: string (guest token)
+    """
+    url = 'https://twitter.com'
+    req = session.prepare_request(requests.Request('GET', url))
+    r = session.send(req, allow_redirects=True, timeout=15)
+    gt = re.search(r'\("gt=(\d+);', r.text)
+    return gt.group(1)
+
+
+def request(url):
+    """
+    Wrapper to send get request with auth headers
+    :param url: string (encoded url including all query params)
+    :return: requests.Response object
+    """
+    _headers = {
+        'authorization': get_auth_token(),
+        'x-guest-token': get_guest_token(),
+    }
+    with session.get(url, headers=_headers) as response:
+        return response
+
+
+def form_query(user, scheduled):
+    """
+    Forms a query string to request for user information
+    :param user: string (Twitter Username/handle to query)
+    :param scheduled: POSIX timestamp as float (lower bound time to query from)
+    :return: A url to query for tweets
+    """
+    query = f"from:{user}"
+    if scheduled is not None:
+        query += f"since:{int(scheduled)}"
+
+    search_options = [('count', 5 if scheduled is None else 100), ('cursor', -1), ('tweet_search_mode', 'live'),
+                      ('include_profile_interstitial_type', '1'), ('include_blocking', '1'),
+                      ('include_blocked_by', '1'), ('include_followed_by', '1'), ('include_want_retweets', '1'),
+                      ('include_mute_edge', '1'), ('include_can_dm', '1'), ('include_can_media_tag', '1'),
+                      ('skip_status', '1'), ('cards_platform', 'Web-12'), ('include_cards', '1'),
+                      ('include_ext_alt_text', 'true'), ('include_quote_count', 'true'), ('include_reply_count', '1'),
+                      ('tweet_mode', 'extended'), ('include_entities', 'true'), ('include_user_entities', 'true'),
+                      ('include_ext_media_color', 'true'), ('include_ext_media_availability', 'true'),
+                      ('send_error_codes', 'true'), ('simple_quoted_tweet', 'true'), ('query_source', 'typed_query'),
+                      ('pc', '1'), ('spelling_corrections', '1'), ('ext', 'mediaStats%2ChighlightedLabel'),
+                      ("q", query)]
+    return "https://api.twitter.com/2/search/adaptive.json" + "?" + urlencode(search_options, quote_via=quote)
+
+
+def read_tweets(json_response):
+    """
+    processes response from twitter into list of json tweets
+    :param json_response: string (json response with tweets)
+    :return: Array[JSON] (Tweet objects)
+    """
+
+    response = json.loads(json_response)
+    feed = []
+    for timeline_entry in response['timeline']['instructions'][0]['addEntries']['entries']:
+        if timeline_entry['entryId'].startswith('sq-I-t-'):
+            if 'tweet' in timeline_entry['content']['item']['content']:
+                _id = timeline_entry['content']['item']['content']['tweet']['id']
+                feed.append(response['globalObjects']['tweets'][_id])
+    return feed
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Read a users tweets')
+    parser.add_argument("-u", "--username", help="Username of persons tweets to collect", required=True)
+    parser.add_argument("-f", "--frequency", type=int, help="polling frequency in minutes", default=10)
+    args = parser.parse_args()
+
+    scheduled_time = None
+    while True:
+        pre_execution_timestamp = datetime.datetime.now().timestamp()
+        url = form_query(args.username, scheduled_time)
+        response = request(url).text
+        for tweet in reversed(read_tweets(response)):
+            print(tweet['created_at'] + " : " + tweet['full_text'])
+        time.sleep(args.frequency * MINUTES_TO_SECONDS)
+        scheduled_time = pre_execution_timestamp
